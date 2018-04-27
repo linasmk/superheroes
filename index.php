db4d17381a4aeac734946b7f8205347602486636
linasmk <linas.mackonis@gmail.com>
2018-04-27 11:52:33 +0200

    Last changes

--- index.php
+++ index.php
@@ -3,11 +3,20 @@ include "includes/header.php";
 
 ?>
 <div class="user-details">
-	<a href="#"><img src="<?php echo $reg_name['profile_pic']; ?>"></a>
+	<a href="<?php echo $usserLoggedIn;?>"><img src="<?php echo $reg_name['profile_pic']; ?>"></a>
 	<br>
+
 	<?php 
 	echo $reg_name['reg_name'] . "<br>" . $reg_name['superpower'] . " " . "<br>" . $reg_name['email'] . "<br>" . "Sign up date: " . $reg_name['singup_date'] . "<br>" . "Number of posts: " . $reg_name['num_posts'] . "<br>" . "Number of Likes: " . $reg_name['num_likes'];
 	?>
+</div>
+
+<div class="main-content">
+	<form action="index.php" method="POST">
+		<textarea name="post_text" id="post_text" placeholder="Wanna post something?"></textarea>
+		<input type="submit" name="post" id="post_button" value="post">
+		<hr>
+	</form>
 </div>
 
 </body>
